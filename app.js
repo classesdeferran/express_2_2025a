5d20d9a89949aa94ce4f034e84a99a3cbe5d3645
Ferran <ferran.docencia@gmail.com>
2025-06-17 10:56:32 +0200

    ruta /pais

--- app.js
+++ app.js
@@ -22,13 +22,43 @@ app.get('/api', (req, res) => {
     res.json(jsonData)
 })
 
-app.get('/api/total-paises', (req, res) => {
+app.get('/api/paises', (req, res) => {
     // Salida esperada :
     // [{"pais": "Italia", "total-ventas": 3000},{"pais": "Francia", "total-ventas": 3000}]
+    let resultado = []
+    let ventasPais = {}
 
+    for (let i = 0; i < jsonData.length; i++) {
+        // datos de cada iteración
+        let pais = jsonData[i].pais
+        let venta = jsonData[i].venta
 
+        // crearemos un objeto cuya clave será el nombre del país
+        // y el valor el total de las ventas
+        if (!ventasPais[pais]) {
+            ventasPais[pais] = 0
+        }
+        // acumulamos las ventas en cada iteración
+        ventasPais[pais] += venta
+    }
 
+    console.log(ventasPais);
 
+    // Añadimos los objetos de cada país al array
+    for (let pais in ventasPais) {
+        resultado.push({
+            "pais": pais,
+            "total-ventas": ventasPais[pais]
+        })
+    }
+    // salida al navegador
+    res.json(resultado)
+    
+})
+
+// Deben salir las ventas por paises en el año 2022
+app.get('api/anyo/2022', (req, res) => {
+//
 })
 
 app.listen(PORT, () => {
